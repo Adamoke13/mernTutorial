bab768f55a875600c753c5f226001e4bff6d44c9
ADAM OKE <ADAM.OKE@LIBERTYMUTUAL.COM>
2017-10-31 20:24:02 -0400

    Step 13 - add edit page and routing

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
 import AddItem from './components/AddItem';
 import IndexItem from './components/IndexItem';
+import EditItem from './components/EditItem';
 import './index.css';
 
 ReactDOM.render(
@@ -12,6 +13,7 @@ ReactDOM.render(
         <Route exact path='/' component={App} />
         <Route path='/add-item' component={AddItem} />
         <Route path='/index' component={IndexItem} />
+        <Route path='/edit/:id' component={EditItem} />
       </div>
   </Router>,
   document.getElementById('root')
