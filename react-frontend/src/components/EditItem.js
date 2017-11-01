15eeb4ac041e4df915d20355e182f2b783a2684a
ADAM OKE <ADAM.OKE@LIBERTYMUTUAL.COM>
2017-10-31 20:32:29 -0400

    Step 14 - update items in the db

--- react-frontend/src/components/EditItem.js
+++ react-frontend/src/components/EditItem.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ItemService from './ItemService';
 
 class EditItem extends Component {
 
   constructor(props) {
       super(props);
-      this.state = {items: ''};
+      this.addItemService = new ItemService();
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.state = {value: '' };
   }
 
   componentDidMount(){
     axios.get('http://localhost:4200/items/edit/'+this.props.match.params.id)
     .then(response => {
-      this.setState({ items: response.data });
+      this.setState({ value: response.data});
     })
     .catch(function (error) {
       console.log(error);
     })
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.addItemService.updateData(this.state.value,this.props.match.params.id);
+    this.props.history.push('/index');
   }
 
   render() {
@@ -24,7 +38,7 @@ class EditItem extends Component {
             <form onSubmit={this.handleSubmit}>
               <label>
                 Edit Item:
-                <input type="text" value={this.state.items.item}  className="form-control"/>
+                <input type="text" value={this.state.value.item} onChange={this.handleChange}  className="form-control"/>
               </label><br/>
               <input type="submit" value="Update" className="btn btn-primary"/>
             </form>
